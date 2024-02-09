77fc0d1c1d64f3267b4b90ea02fec7fbc30d6163
Andrew Parker <andrew.parker@kearsarge.org>
2024-02-09 08:19:30 -0500

    added a thing

--- Game.py
+++ Game.py
@@ -2,3 +2,10 @@ import pygame, sys, math, random
 from Arm import *
 from leg import *
 from Player import *
+
+pygame.init()
+
+clock = pygame.time.Clock();
+
+size = [900, 700]
+screen = pygame.display.set_mode(size)

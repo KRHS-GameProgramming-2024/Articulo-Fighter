db418a5e9831597b39fdc9a2b6558834c4623fb5
Andrew Parker <andrew.parker@kearsarge.org>
2024-02-14 08:08:41 -0500

    background added

--- Game.py
+++ Game.py
@@ -9,3 +9,15 @@ clock = pygame.time.Clock();
 
 size = [900, 700]
 screen = pygame.display.set_mode(size)
+
+bgimage = pygame.image.load("Images/Stages/temp_background.png")
+bgrect = bgimage.get_rect()
+
+while True:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            sys.exit();
+            
+    screen.blit(bgimage, bgrect)
+    pygame.display.flip()
+    clock.tick(60)

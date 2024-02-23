0534572ad5ae9f5d9dcf915c81d65c055324d06a
Andrew Parker <andrew.parker@kearsarge.org>
2024-02-23 07:58:07 -0500

    New things

--- Game.py
+++ Game.py
@@ -2,6 +2,7 @@ import pygame, sys, math, random
 from Arm import *
 from leg import *
 from Player import *
+from PlayerCollide import *
 
 pygame.init()
 
@@ -10,14 +11,39 @@ clock = pygame.time.Clock();
 size = [900, 700]
 screen = pygame.display.set_mode(size)
 
-bgimage = pygame.image.load("Images/Stages/temp_background.png")
-bgrect = bgimage.get_rect()
+counter = 0;
+player = Player(4, [900/2, 700/2])
 
 while True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             sys.exit();
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                player.goKey("left")
+            elif event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                player.goKey("right")
+            elif event.key == pygame.K_w or event.key == pygame.K_UP:
+                player.goKey("up")
+            elif event.key == pygame.K_s or event.key == pygame.K_DOWN:
+                player.goKey("down")
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                player.goKey("sleft")
+            elif event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                player.goKey("sright")
+            elif event.key == pygame.K_w or event.key == pygame.K_UP:
+                player.goKey("sup")
+            elif event.key == pygame.K_s or event.key == pygame.K_DOWN:
+                player.goKey("sdown")
+
+
+    player.move()
+    
+    bgimage = pygame.image.load("Images/Stages/temp_background.png")
+    bgrect = bgimage.get_rect()
             
     screen.blit(bgimage, bgrect)
+    screen.blit(player.image, player.rect)
     pygame.display.flip()
     clock.tick(60)
